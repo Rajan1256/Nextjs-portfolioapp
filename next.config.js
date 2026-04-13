efb5dbe0e09973fa0368ac9d779fbc08c75fd4db
RajanAxin <rajan@axinenterprise.com>
2026-04-13 12:26:25 +0530

    get a fresh repo work

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-//module.exports = nextConfig
-
-module.exports = {
-  reactStrictMode: true,
-  images: {
-      domains: ["q3hwxssz.directus.app"],
-      //formats: ["image/webp"],
-  },
 };
+
+module.exports = nextConfig;
